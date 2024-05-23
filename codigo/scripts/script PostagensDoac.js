70759e4e3e1ce772a75bdd118d07b9ad48116508
fernandassp <fernandassp1907@gmail.com>
2024-05-23 18:33:27 -0300

    foto do pet doação + css das divs

--- codigo/scripts/script PostagensDoac.js
+++ codigo/scripts/script PostagensDoac.js
@@ -3,6 +3,7 @@
 
 // perguntas: como incluir foto enviada pelo usuario
 
+/*
 document.addEventListener("DOMContentLoaded", function () {
 
   function leDadosAdoc() {
@@ -38,7 +39,7 @@ document.addEventListener("DOMContentLoaded", function () {
     for (i = 0; i < objDadosAdoc.petAdoc.length; i++) {
       let novaAdoc = document.createElement('div');
       novaAdoc.classList.add('novaadoc');
-      novaAdoc.innerHTML = `  <img src="${objDadosAdoc.petAdoc[i].fotoAd}" alt="Foto do Pet" width="100"> <p>${objDadosAdoc.petAdoc[i].carac}</p>`; // img
+      novaAdoc.innerHTML = `<img id="displayImage" src="" alt="Foto do Pet" width="100"> <p>${objDadosAdoc.petAdoc[i].carac}</p>`; // img
       let btnDetalhesAdoc = document.createElement('button');
       btnDetalhesAdoc.innerHTML = '<a href="#">Ver detalhes</a>';
       btnDetalhesAdoc.classList.add('btnDetalhes');
@@ -81,8 +82,86 @@ document.addEventListener("DOMContentLoaded", function () {
  
   imprimeAdoc();
 
-})
+}) */
 
+document.addEventListener("DOMContentLoaded", function () {
+
+  function leDadosAdoc() {
+    let strDadosAdoc = localStorage.getItem('db2');
+    let objDadosAdoc = {};
+
+    if (strDadosAdoc) {
+      objDadosAdoc = JSON.parse(strDadosAdoc);
+    }
+    else {
+      objDadosAdoc = {
+        petAdoc: [{
+          carac: "Filhote de yorkshire, muito dócil, macho, porte pequeno.",
+          especie: "cachorro",
+          idadepet: "filhote - 3 meses",
+          raca: "yorkshire",
+          enderecoAdoc: "bairro União",
+          emailAd: "laura@gmail.com",
+          telAd: "31 9874-5621",
+          fotoAd: ""
+        }]
+      }
+    }
+    return objDadosAdoc;
+  }
+
+  function salvaDadosAdoc(dados) {
+    localStorage.setItem('db2', JSON.stringify(dados));
+  }
+
+  function imprimeAdoc() {
+    let tela = document.getElementById('divDoacoes');
+    let objDadosAdoc = leDadosAdoc();
+    for (let i = 0; i < objDadosAdoc.petAdoc.length; i++) {
+      let novaAdoc = document.createElement('div');
+      novaAdoc.classList.add('novaadoc');
+      novaAdoc.innerHTML = `<img src="${objDadosAdoc.petAdoc[i].fotoAd}" alt="Foto do Pet" width="200"> <p>${objDadosAdoc.petAdoc[i].carac}</p>`;
+      let btnDetalhesAdoc = document.createElement('button');
+      btnDetalhesAdoc.innerHTML = '<a href="#">Ver detalhes</a>';
+      btnDetalhesAdoc.classList.add('btnDetalhes');
+      novaAdoc.appendChild(btnDetalhesAdoc);
+      tela.appendChild(novaAdoc);
+    }
+  }
+
+  function incluirAdoc(base64Image) {
+    let objDadosAdoc = leDadosAdoc();
+    let strCaracAdoc = document.getElementById('carac-adocao').value;
+    let strEmailAdoc = document.getElementById('adoc-email').value;
+    let strTelAdoc = document.getElementById('adoc-tel').value;
+    let strEspecie = document.getElementById('especie').value;
+    let strIdade = document.getElementById('idadepet').value;
+    let strRaca = document.getElementById('raca').value;
+    let strEndereco = document.getElementById('endereco-adoc').value;
+
+    let novaAdoc = {
+      carac: strCaracAdoc,
+      especie: strEspecie,
+      idadepet: strIdade,
+      raca: strRaca,
+      enderecoAdoc: strEndereco,
+      emailAd: strEmailAdoc,
+      telAd: strTelAdoc,
+      fotoAd: base64Image
+    };
+
+    objDadosAdoc.petAdoc.push(novaAdoc);
+    salvaDadosAdoc(objDadosAdoc);
+
+    // Atualiza dados na tela
+    imprimeAdoc();
+  }
+
+  
+  
+  // Inicializa exibindo as imagens já salvas
+  imprimeAdoc();
+});
 
 
 
--- codigo/scripts/scriptDoac.js
+++ codigo/scripts/scriptDoac.js
@@ -3,6 +3,7 @@
 
 // perguntas: como incluir foto enviada pelo usuario
 
+/* - código antigo 
 document.addEventListener("DOMContentLoaded", function () {
 
   function leDadosAdoc() {
@@ -15,14 +16,15 @@ document.addEventListener("DOMContentLoaded", function () {
     else {
       objDadosAdoc = {
         petAdoc: [{
-            carac: "Filhote de yorkshire, muito dócil, macho, porte pequeno.",
-            especie: "cachorro",
-            idadepet: "filhote - 3 meses",
-            raca: "yorkshire",
-            enderecoAdoc: "bairro União",
-            emailAd: "laura@gmail.com",
-            telAd: "31 9874-5621",
-            fotoAd: "" }]
+          carac: "Filhote de yorkshire, muito dócil, macho, porte pequeno.",
+          especie: "cachorro",
+          idadepet: "filhote - 3 meses",
+          raca: "yorkshire",
+          enderecoAdoc: "bairro União",
+          emailAd: "laura@gmail.com",
+          telAd: "31 9874-5621",
+          fotoAd: ""
+        }]
       }
     }
     return objDadosAdoc;
@@ -38,7 +40,7 @@ document.addEventListener("DOMContentLoaded", function () {
     for (i = 0; i < objDadosAdoc.petAband.length; i++) {
       let novaAdoc = document.createElement('div');
       novaAdoc.classList.add('novaadoc');
-      novaAdoc.innerHTML = `  <img src="${objDadosAdoc.petAdoc[i].fotoAd}" alt="Foto do Pet" width="100"> <p>${objDadosAdoc.petAdoc[i].carac}</p>`; // img
+      novaAdoc.innerHTML = `<img id="displayImage" src="" alt="Foto do Pet" width="100"> <p>${objDadosAdoc.petAdoc[i].carac}</p>`; // img
       let btnDetalhesAdoc = document.createElement('button');
       btnDetalhesAdoc.innerHTML = '<a href="#">Ver detalhes</a>';
       btnDetalhesAdoc.classList.add('btnDetalhes');
@@ -49,14 +51,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function incluirAdoc() {
     let objDadosAdoc = leDadosAdoc();
-    let strCaracAdoc = document.getElementById('carac-adocao').value;    
+    let strCaracAdoc = document.getElementById('carac-adocao').value;
     let strEmailAdoc = document.getElementById('adoc-email').value;
     let strTelAdoc = document.getElementById('adoc-tel').value;
     let strEspecie = document.getElementById('especie').value;
     let strIdade = document.getElementById('idadepet').value;
     let strRaca = document.getElementById('raca').value;
     let strEndereco = document.getElementById('endereco-adoc').value;
-   // let fotoAd = document.getElementById('enviar-arquivoAd').files[0]; //
 
 
     let novaAdoc = {
@@ -75,38 +76,156 @@ document.addEventListener("DOMContentLoaded", function () {
 
     //att dados da tela
     imprimeAdoc();
-  } 
+  }
 
   // botão enviar
   // dps: validar se usuario enviou foto do pet tbm
-  
-    let strCaracAdoc = document.getElementById('carac-adocao');    
-    let strEmailAdoc = document.getElementById('adoc-email');
-    let strTelAdoc = document.getElementById('adoc-tel');
-    let strEspecie = document.getElementById('especie');
-    let strIdade = document.getElementById('idadepet');
-    let strEndereco = document.getElementById('endereco-adoc');
-  var formAdoc = document.getElementById("formAdoc");
+
+  let strCaracAdoc = document.getElementById('carac-adocao');
+  let strEmailAdoc = document.getElementById('adoc-email');
+  let strTelAdoc = document.getElementById('adoc-tel');
+  let strEspecie = document.getElementById('especie');
+  let strIdade = document.getElementById('idadepet');
+  let strEndereco = document.getElementById('endereco-adoc');
+
+  const formAdoc = document.getElementById("formAdoc"); // id do form
+  const fileInput = document.getElementById("enviar-arquivoAd"); // input type file
+  const displayImage = document.getElementById('displayImage'); // id da imagem
+
 
   formAdoc.addEventListener('submit', (e) => {
+
+    const file = fileInput.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function (event) {
+        const base64Image = event.target.result;
+        localStorage.setItem('uploadedImage', base64Image);
+        displayImage.src = base64Image;
+        alert('Imagem carregada e salva com sucesso!');
+      }
+      reader.readAsDataURL(file);
+    } else {
+      alert('Por favor, selecione uma imagem para fazer upload.');
+    }
+
 
     if (strCaracAdoc.value == "" || strEmailAdoc.value == "" || strTelAdoc.value == "" || strEspecie.value == "" || strIdade.value == "" || strEndereco.value == "") {
       alert("Preencha todos os campos obrigatórios!");
       e.preventDefault();
       return;
     }
-    
-    else{
+
+    else {
       alert("Enviado!");
       incluirAdoc();
     }
   })
 
-  
-   
+  // Carregar a imagem do local storage, se existir
+  const savedImage = localStorage.getItem('uploadedImage');
+  if (savedImage) {
+      displayImage.src = savedImage;
+  }
+
+
+
 })
 
 
+*/
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  function leDadosAdoc() {
+    let strDadosAdoc = localStorage.getItem('db2');
+    let objDadosAdoc = {};
+
+    if (strDadosAdoc) {
+      objDadosAdoc = JSON.parse(strDadosAdoc);
+    }
+    else {
+      objDadosAdoc = {
+        petAdoc: [{
+          carac: "Filhote de yorkshire, muito dócil, macho, porte pequeno.",
+          especie: "cachorro",
+          idadepet: "filhote - 3 meses",
+          raca: "yorkshire",
+          enderecoAdoc: "bairro União",
+          emailAd: "laura@gmail.com",
+          telAd: "31 9874-5621",
+          fotoAd: ""
+        }]
+      }
+    }
+    return objDadosAdoc;
+  }
+
+  function salvaDadosAdoc(dados) {
+    localStorage.setItem('db2', JSON.stringify(dados));
+  }
+
+
+
+  function incluirAdoc(base64Image) {
+    let objDadosAdoc = leDadosAdoc();
+    let strCaracAdoc = document.getElementById('carac-adocao').value;
+    let strEmailAdoc = document.getElementById('adoc-email').value;
+    let strTelAdoc = document.getElementById('adoc-tel').value;
+    let strEspecie = document.getElementById('especie').value;
+    let strIdade = document.getElementById('idadepet').value;
+    let strRaca = document.getElementById('raca').value;
+    let strEndereco = document.getElementById('endereco-adoc').value;
+
+    let novaAdoc = {
+      carac: strCaracAdoc,
+      especie: strEspecie,
+      idadepet: strIdade,
+      raca: strRaca,
+      enderecoAdoc: strEndereco,
+      emailAd: strEmailAdoc,
+      telAd: strTelAdoc,
+      fotoAd: base64Image
+    };
+
+    objDadosAdoc.petAdoc.push(novaAdoc);
+    salvaDadosAdoc(objDadosAdoc);
+
+  }
+
+  // botão enviar
+
+  const formAdoc = document.getElementById("formAdoc"); // id do form
+  const fileInput = document.getElementById("enviar-arquivoAd"); // input type file
+
+  formAdoc.addEventListener('submit', (e) => {
+    
+    
+    if (document.getElementById('carac-adocao').value == "" || document.getElementById('adoc-email').value == "" || document.getElementById('adoc-tel').value == "" || document.getElementById('especie').value == "" || document.getElementById('idadepet').value == "" || document.getElementById('endereco-adoc').value == "") {
+      alert("Preencha todos os campos obrigatórios!");
+      e.preventDefault();
+      return;
+    }
+    else{
+      alert("Enviado!");
+    }
+
+    const file = fileInput.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function (event) {
+        const base64Image = event.target.result;
+        incluirAdoc(base64Image);
+      }
+      reader.readAsDataURL(file);
+    } else {
+      alert('Por favor, selecione uma imagem para fazer upload.');
+    }
+  });
+
+});
 
 
 
