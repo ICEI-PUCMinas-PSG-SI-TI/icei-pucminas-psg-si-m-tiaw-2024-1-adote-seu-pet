96e624275359fa099074b136e7da5b5a5f224ff0
fernandassp <fernandassp1907@gmail.com>
2024-06-19 18:36:57 -0300

    linkagem de paginas, correções

---
+++ codigo/scripts/dicasdecuidados.js
@@ -0,0 +1,30 @@
+function mostrarInformacoes() {
+    var informacoes = document.getElementById("informacoes");
+    if (informacoes.style.display === "none") {
+        informacoes.style.display = "block";
+    } else {
+        informacoes.style.display = "none";
+    }
+}
+
+function searchcontainer(termo) {
+    let historico = JSON.parse(localStorage.getItem('historicoPesquisas')) || [];
+
+    historico.push(termo);
+
+    localStorage.setItem('historicoPesquisas', JSON.stringify(historico));
+}
+
+function recuperarHistoricoDePesquisas() {
+    const historico = JSON.parse(localStorage.getItem('historicoPesquisas')) || [];
+
+    return historico;
+}
+
+adicionarPesquisaAoHistorico('ração para cachorro');
+adicionarPesquisaAoHistorico('brinquedos para gatos');
+adicionarPesquisaAoHistorico('veterinário São Paulo');
+
+// Recuperar histórico de pesquisas
+const historicoRecuperado = recuperarHistoricoDePesquisas();
+console.log('Histórico de Pesquisas:', historicoRecuperado);
